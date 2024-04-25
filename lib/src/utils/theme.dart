87cd70bb5d86140568e6dc124aa9cca07a723366
Marel Agonglo <agonglomarel@mail.com>
2024-04-26 00:35:37 +0100

    Custom theme in flutter

---
+++ lib/src/utils/theme.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+
+  static ThemeData lighttheme = ThemeData(brightness: Brightness.light);
+
+  static ThemeData darttheme = ThemeData(brightness: Brightness.dark);
+}
